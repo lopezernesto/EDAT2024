3873064e0a35e4d03e2dbc6d2761790e0ae8ddf3
Ernesto López <ernesto.lopez@est.fi.uncoma.edu.ar>
2024-07-09 10:27:05 -0300

    AVL funcionando

--- src/tests/conjuntistas/TestAVL.java
+++ src/tests/conjuntistas/TestAVL.java
@@ -5,16 +5,38 @@ import conjuntistas.dinamicas.ArbolAVL;
 public class TestAVL {
     public static void main(String[] args) {
         ArbolAVL arbol = new ArbolAVL();
-        System.out.println(arbol.insertar(5));
-        System.out.println(arbol.insertar(4));
-        System.out.println(arbol.insertar(8));
-        System.out.println(arbol.insertar(6));
-        System.out.println(arbol.toString());
-        System.out.println("_---------");
-        System.out.println(arbol.insertar(7));
-        System.out.println(arbol.insertar(10));
+
+        // arbol.insertar(7);
+        // arbol.insertar(8);
+        // arbol.insertar(5);
+
+        // arbol.insertar(6);
+
+        // arbol.insertar(9);
+
+        arbol.insertar(15);
+        arbol.insertar(10);
+        arbol.insertar(20);
+        arbol.insertar(5);
+        arbol.insertar(12);
+        arbol.insertar(25);
+        arbol.insertar(3);
+        arbol.insertar(8);
 
         System.out.println(arbol.toString());
+        arbol.eliminar(15);
+        arbol.insertar(4);
+        arbol.insertar(6);
+        arbol.insertar(11);
+        System.out.println(arbol.toString());
+
+        // ------ 15
+        // ----- / \
+        // --- 10 - 20
+        // --- / \ - \
+        // ---5 -12 - 25
+        // --/ \
+        // -3 - 8
 
     }
 }
